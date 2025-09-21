5ca6f997c4f43d3064e34aa4697cb4c77b64b08f
Joshua Kirby <jojokirby88@gmail.com>
2025-09-22 07:51:52 +0900

    Made deployment work.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { FoodAdder } from "./_components/page/food-adder"
 import { MacroBarChart } from "./_components/page/macro-bar-chart"
 import { TodayEntries } from "./_components/page/today-entries/today-entries"
@@ -9,7 +10,9 @@ const Page = () => {
 
 			<TodayEntries />
 
-			<FoodAdder />
+			<Suspense>
+				<FoodAdder />
+			</Suspense>
 		</div>
 	)
 }
